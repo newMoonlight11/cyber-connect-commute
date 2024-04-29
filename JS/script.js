068f93c7c5329b7e43ecf5e6b81e057253cb75ed
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2024-04-29 10:43:39 -0500

    login-register

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,16 @@
-// Selecciona todos los enlaces con la clase 'texto-con-sombra'
 const enlacesConSombra = document.querySelectorAll('.texto-con-sombra');
 
-// Itera sobre cada enlace y agrega event listeners
 enlacesConSombra.forEach(enlace => {
-    // Agrega un event listener para el evento 'mouseover'
     enlace.addEventListener('mouseover', () => {
-        // Agrega la clase 'con-sombra' cuando el mouse está encima
         enlace.classList.add('con-sombra');
     });
 
-    // Agrega un event listener para el evento 'mouseout'
     enlace.addEventListener('mouseout', () => {
-        // Remueve la clase 'con-sombra' cuando el mouse sale
         enlace.classList.remove('con-sombra');
     });
 });
 
-// Función para cargar el contenido de la página cuando se hace clic en un enlace del menú
+
 function cargarContenido(url) {
     fetch(url)
         .then(response => response.text())
@@ -25,11 +19,10 @@ function cargarContenido(url) {
         });
 }
 
-// Event listener para los enlaces del menú
+
 document.querySelectorAll('.link').forEach(enlace => {
     enlace.addEventListener('click', function(e) {
-        e.preventDefault(); // Evitar que el navegador cargue la página
+        e.preventDefault(); 
         cargarContenido(this.getAttribute('href'));
     });
 });
-
